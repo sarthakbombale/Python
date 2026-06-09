89724f918be116153cbdab77ec5c7a28dcb1cb1a
sarthakbombale <sarthakbomble250@gmail.com>
2026-06-09 20:51:40 +0530

    Practice

---
+++ string.py
@@ -0,0 +1,21 @@
+name = "sarthak"
+
+print(len(name))
+
+nameshort = name[0:3]
+
+print(nameshort)
+
+
+# str functions
+print(name.startswith("sar"))
+print(name.endswith("ak"))
+print(name.capitalize())
+
+
+a = "5"
+b = int(a)
+print(type(b))
+
+
+
