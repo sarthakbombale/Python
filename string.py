2861c78907f58bfc9f2541b9f3d33a4f9b945099
sarthakbombale <sarthakbomble250@gmail.com>
2026-06-09 20:55:27 +0530

    Practice

--- string.py
+++ string.py
@@ -18,4 +18,16 @@ b = int(a)
 print(type(b))
 
 
+name = input("What is your name? ")
+age = input("What is your age? ")
+where = input("Where do you live? ")
+percentage = input("What is your percentage? ")
+ok = input("ok so your name is " + name + " and your age is " + age + " and you live in " + where + " and your percentage is " + percentage)
+print("Your name is " + name)
+print("Your age is " + age)
+print("You live in " + where)
+print("Your percentage is " + percentage)
+
+
+
 
