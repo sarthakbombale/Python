b64732216e02c074ef786b0ee166b6ca257a8505
Sarthak Bombale <sarthakbomble250@gmail.com>
2026-07-02 11:31:22 +0530

    Add car tuple and airplane seats dictionary

---
+++ _tuple.py
@@ -0,0 +1,13 @@
+car_tuple = ('ferrari','lambo','audy','GT')
+
+print(car_tuple)
+
+
+airplane_seats ={
+    (5,"A"):"Alice Doe",
+    (2, "B"): "Jon Jones"
+}
+
+print(airplane_seats[(5,"A")])
+
+print(airplane_seats[(2,"B")])
