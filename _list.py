6a32877ac1cee77dda125554ea9ad5c4c7491f7f
Sarthak Bombale <sarthakbomble250@gmail.com>
2026-07-02 11:18:21 +0530

    Enhance _list.py with list mutability examples
    Added examples of list mutability and fruit manipulation.

--- _list.py
+++ _list.py
@@ -7,3 +7,11 @@ print(fruits[2])
 
 print(fruits[1:4]) # it will print from 0 to 3 index    
 # Unlike string  list is mutable
+
+fruits =  ['banana','apple','kiwi']
+
+print(fruits[0])
+fruits[0] = 'strawberry'
+fruits.append('orange')
+
+print(fruits)
