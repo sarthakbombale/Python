4ce296027efc5119684123ccbabaf1045eeaa9f2
Sarthak Bombale <sarthakbomble250@gmail.com>
2026-07-03 17:46:13 +0530

    Create _functions.py

---
+++ _functions.py
@@ -0,0 +1,39 @@
+def my_function():
+    print("Hello My_Function")
+my_function()
+
+def greet_by_name(name):
+    print(f'Hellow {name} Bhai whats up!! ')
+greet_by_name("Harry")
+
+def calculate_tax(bill_amount):
+    tax = bill_amount * 0.18
+    return tax
+    
+final_tax = calculate_tax(500)
+print(f'Your tax amount is {final_tax}')
+
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit -32) * 5 / 7
+    
+print(fahrenheit_to_celsius(32))
+print(fahrenheit_to_celsius(64))
+
+def get_greeting():
+    return "WE GET GRETINGS"
+greeting = get_greeting()
+print(greeting)
+
+print(get_greeting())
+
+def is_password_safe(password):
+    if len(password) >= 8:
+      return True
+    else:
+      return False
+
+user_input = "pass123"
+if is_password_safe(user_input) == True:
+    print("password accepted")
+else:
+    print("Registration failed: Password is too short.")
