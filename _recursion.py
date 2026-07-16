c0db340edb2c14f12461c38765821bf24b2d433b
Sarthak Bombale <sarthakbomble250@gmail.com>
2026-07-16 09:44:30 +0530

    Update _recursion.py

--- _recursion.py
+++ _recursion.py
@@ -85,3 +85,46 @@ def find_max(numbers):
     return numbers[0] if numbers[0] > sub_max else sub_max
 
 print(find_max([3, 8, 1, 9, 2]))
+def multiply_list(numbers):
+    if len(numbers) == 0:
+        return 1
+    return numbers[0] * multiply_list(numbers[1:])
+
+print(multiply_list([1, 2, 3, 4]))
+
+def get_factorial(n):
+    if n <= 1:
+        return 1
+    return n * get_factorial(n - 1)
+
+print(get_factorial(5))
+
+def reverse_list(items):
+    if len(items) == 0:
+        return items
+    return [items[-1]] + reverse_list(items[:-1])
+
+print(reverse_list(["a", "b", "c"]))
+
+def count_digits(n):
+    if n < 10:
+        return 1
+    return 1 + count_digits(n // 10)
+
+print(count_digits(54321))
+
+def remove_vowels(text):
+    if not text:
+        return ""
+    char = "" if text[0].lower() in "aeiou" else text[0]
+    return char + remove_vowels(text[1:])
+
+print(remove_vowels("javascript"))
+
+def find_min(numbers):
+    if len(numbers) == 1:
+        return numbers[0]
+    sub_min = find_min(numbers[1:])
+    return numbers[0] if numbers[0] < sub_min else sub_min
+
+print(find_min([5, 2, 9, 1, 7]))
